050bb6ed954f5ae6afc32d7d4208deecb58bd3fc
Jose Galaz <jdgalaz@uc.cl>
2018-03-19 01:30:18 -0300

    enlarge domain to include missing pois and fix fourier filter, #132

--- validation/04_tohoku2011/tsunamilab/main.js
+++ validation/04_tohoku2011/tsunamilab/main.js
@@ -1,31 +1,31 @@
-let w = parseInt(1350);
+let w = parseInt(2101);
 let h = parseInt(1801);
 
 let data = {
     bathymetry: '../data/bathymetry',
     earthquake: '../data/earthquake.csv',
     coordinates: 'spherical',
-    waveWidth: w,
-    waveHeight: h,
-    displayWidth:  w,
-    displayHeight: h,
+    waveWidth: w/2,
+    waveHeight: h/2,
+    displayWidth:  w/2,
+    displayHeight: h/2,
     xmin : 135,
-    xmax :  180,
-    ymin :  -10,
+    xmax :  215,
+    ymin :  -20,
     ymax : 50
 }
 
 let output = {
-    stopTime: 60*60*24,
-    displayOption: 'max heights',
+    stopTime: 60*60*12,
+    displayOption: 'heights',
     pois: {
         '21401': {location: [152.583, 42.617]},
         '21413': {location: [152.132, 30.533]},
         '21414': {location: [178.219, 48.968]},
         '21418': {location: [148.645, 38.727]},
         '21419': {location: [155.717, 44.435]},
-        '51407': {location: [-156.545+180, 19.57]},
-        '51425': {location: [-176.32+180, -9.517]},
+        '51407': {location: [-156.545+360, 19.57]},
+        '51425': {location: [-176.32+360, -9.517]},
         '52403': {location: [145.52, 4.02]},
         '52406': {location: [164.977, -5.307]},
         '55012': {location: [158.453, -15.66]}
@@ -45,6 +45,10 @@ let lifeCycle = {
         if(model.discretization.stepNumber % 100==0){
             console.log(model.currentTime/60/60, controller.stopTime/60/60);
         }
+    },
+
+    dataWasLoaded: (model)=>{
+        document.body.appendChild(model.canvas);
     }
 }
 
