9f9f1a0ce8475805f941f940f334ea66b5bbf0f5
Jose Galaz <jdgalaz@uc.cl>
2018-03-23 04:54:17 -0300

    new domain and pois cover japan to chile, #132

--- validation/04_tohoku2011/tsunamilab/main.js
+++ validation/04_tohoku2011/tsunamilab/main.js
@@ -1,5 +1,5 @@
-let w = parseInt(2401);
-let h = parseInt(2101);
+let w = parseInt(3101);
+let h = parseInt(1401);
 
 let data = {
     bathymetry: '../data/bathymetry',
@@ -8,28 +8,38 @@ let data = {
     coordinates: 'spherical',
     waveWidth: w/4,
     waveHeight: h/4,
-    displayWidth:  w/4,
-    displayHeight: h/4,
+    displayWidth:  w/3,
+    displayHeight: h/3,
     xmin : 135,
-    xmax :  215,
+    xmax :  290,
     ymin :  -20,
     ymax : 50
 }
 
 let output = {
-    stopTime: 60*60*12,
-    displayOption: 'heights',
-    pois: {
-        '21401': {location: [152.583, 42.617]},
-        '21413': {location: [152.132, 30.533]},
-        '21414': {location: [178.219, 48.968]},
-        '21418': {location: [148.645, 38.727]},
-        '21419': {location: [155.717, 44.435]},
-        '51407': {location: [-156.545+360, 19.57]},
-        '51425': {location: [-176.32+360, -9.517]},
-        '52403': {location: [145.52, 4.02]},
-        '52406': {location: [164.977, -5.307]},
-        '55012': {location: [158.453, -15.66]}
+    stopTime: 60*60*24,
+    displayOption: 'arrival times',
+    pois:{
+        '21414': {location:[178.219,48.968]},
+        '21419': {location:[155.717,44.435]},
+        '21401': {location:[152.583,42.617]},
+        '21418': {location:[148.645,38.727]},
+        '21413': {location:[152.132,30.533]},
+        '52403': {location:[145.52,4.02]},
+        '52406': {location:[164.977,-5.307]},
+        '55012': {location:[158.453,-15.664]},
+        '51425': {location:[183.68,-9.517]},
+        '51407': {location:[203.455,19.57]},
+        '46404': {location:[231.267,45.853]},
+        '46407': {location:[231.168,42.682]},
+        '46411': {location:[232.933,39.333]},
+        '46412': {location:[239.437,32.492]},
+        '43412': {location:[253.03300000000002,16.045]},
+        '43413': {location:[259.91700000000003,11.012]},
+        '32411': {location:[269.12,4.953]},
+        '32412': {location:[273.626,-17.984]},
+        '32413': {location:[266.483,-7.406]},
+        '32401': {location:[286.579,-20.474]},
     }
 };
 
