26adde3d3f8548665b8c82f93900339b26800a87
Jose Galaz <jdgalaz@uc.cl>
2018-04-15 21:59:40 -0300

    new locations and comparison for 2015 event, #147

--- validation/06_illapel2015/tsunamilab/main.js
+++ validation/06_illapel2015/tsunamilab/main.js
@@ -21,25 +21,18 @@ let output = {
     displayOption: 'heights',
     pois:{
         '21414': {location:[178.219,48.968]},
-        '21419': {location:[155.717,44.435]},
-        '21401': {location:[152.583,42.617]},
-        '21418': {location:[148.645,38.727]},
-        '21413': {location:[152.132,30.533]},
         '52403': {location:[145.52,4.02]},
         '52406': {location:[164.977,-5.307]},
-        '55012': {location:[158.453,-15.664]},
         '51425': {location:[183.68,-9.517]},
         '51407': {location:[203.455,19.57]},
         '46404': {location:[231.267,45.853]},
         '46407': {location:[231.168,42.682]},
         '46411': {location:[232.933,39.333]},
-        '46412': {location:[239.437,32.492]},
         '43412': {location:[253.03300000000002,16.045]},
         '43413': {location:[259.91700000000003,11.012]},
-        '32411': {location:[269.12-360,4.953]},
-        '32412': {location:[273.626-360,-17.984]},
-        '32413': {location:[266.483-360,-7.406]},
-        '32401': {location:[286.579-360,-20.474]},
+        '32411': {location:[269.12,4.953]},
+        '32412': {location:[273.626,-17.984]},
+        '32401': {location:[286.579,-20.474]},
     }
     
 };
@@ -53,7 +46,7 @@ let lifeCycle = {
         controller.downloadAllPois();    
     },
 
-    modelStepDidFinish: (model, controller) =>{
+    modelStepDidFinish: (model  , controller) =>{
         if(model.discretization.stepNumber % 100==0){
             console.log(model.currentTime/60/60, controller.stopTime/60/60);
         }
