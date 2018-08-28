932f35b138d0a63f75643e3c9d05f62fd9394275
Jose Galaz <jdgalaz@uc.cl>
2018-08-28 19:37:05 -0300

    run script for easywave, 2015 case #4

--- validation/06_illapel2015/easywave/run.sh
+++ validation/06_illapel2015/easywave/run.sh
@@ -1,1 +1,1 @@
-/usr/local/easywave/bin/easywave -grid ../data/bathymetry.grd -source ../data/fault.flt -time 60 -poi ../data/pois -poi_report
+/usr/local/easywave/bin/easywave -grid ../data/bathymetry.grd -source ../data/fault.flt -time 180 -poi ../data/pois -poi_report
