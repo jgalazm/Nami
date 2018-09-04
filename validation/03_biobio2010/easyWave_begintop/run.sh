829114214a70265207e0bc83661eab62b474f888
Jose Galaz <jdgalaz@uc.cl>
2018-09-04 03:10:41 -0300

    delete unnecesary geoclaw and easywave folders, #11

--- validation/03_biobio2010/easyWave_begintop/run.sh
+++
@@ -1,1 +0,0 @@
-easywave -grid ../data/gebco10mins.grd -source fault1.flt -time 480 -poi pois -poi_report
--- validation/03_biobio2010/easyWave_center/clean.sh
+++
@@ -1,5 +0,0 @@
-rm -rf eWave*
-rm -rf easywave.log
-rm -rf error.msg
-rm -rf *.png
-rm -rf *.lst
